a0395038f09f567f9b958c2f42337d7732554c12
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-11 19:46:16 +0100

    started work on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,24 @@
+import "../styles/NavBar.css";
+
 import React, { Component } from "react";
 
 class NavBar extends Component {
+  NavItems() {
+    return (
+      <div className="navItems">
+        <ul>
+          <li className="menuItem">Display resume</li>
+          <li className="menuItem">Edit resume</li>
+        </ul>
+      </div>
+    );
+  }
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div className="navBar">
+        <this.NavItems />
+      </div>
+    );
   }
 }
 
