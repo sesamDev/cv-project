e543db6cb30df785c1456f5b9fea58560857aa7f
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-13 12:33:59 +0100

    Add forms for adding experiances

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,42 +2,96 @@ import "../styles/Form.css";
 
 import React, { Component } from "react";
 
-// firstName: "Sebastian",
-//           lastName: "",
-//           adress: "",
-//           zipCode: "",
-//           city: "",
-//           phone: "",
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobs: [],
+    };
+  }
+  increaseJobForms = () => {
+    this.setState({ jobs: [].concat(this.state.jobs, this.experianceForm()) });
+  };
+  appendExperianceForm = () => {
+    return (
+      <div>
+        {Object.keys(this.state.jobs).map((job) => {
+          return <div key={job}>{this.state.jobs[job]}</div>;
+        })}
+      </div>
+    );
+  };
+  experianceForm = () => {
+    return (
+      <div className="experianceForm">
+        <div>
+          <label htmlFor="employer">Employer</label>
+          <input type="text" id="employer" />
+        </div>
+        <div>
+          <label htmlFor="role">Role</label>
+          <input type="text" id="role" />
+        </div>
+        <div>
+          <label htmlFor="startDate">Start Date</label>
+          <input type="date" id="startDate" />
+        </div>
+        <div>
+          <label htmlFor="endDate">End Date</label>
+          <input type="date" id="endDate" />
+        </div>
+      </div>
+    );
+  };
+  experiance = () => {
+    const experiance = this.props.user.experiance;
+    //Can be used in print view later
+    // const list = Object.keys(experiance).map((entry) => {
+    //   return (
+    //     <ul key={entry}>
+    //       {Object.keys(experiance[entry]).map((keys) => {
+    //         return <li key={keys}>{experiance[entry][keys]}</li>;
+    //       })}
+    //     </ul>
+    //   );
+    // });
+    return null;
+  };
   render() {
     return (
       <div>
         <form>
-          <div class="personalInfo">
+          <div className="personalInfo">
             <div>
-              <label for="firstName">First name</label>
+              <label htmlFor="firstName">First name</label>
               <input type="text" name="firstName" id="firstName" />
             </div>
             <div>
-              <label for="lastName">Last name</label>
+              <label htmlFor="lastName">Last name</label>
               <input type="text" name="lastName" id="lastName" />
             </div>
             <div>
-              <label for="adress">Address</label>
+              <label htmlFor="adress">Address</label>
               <input type="text" name="adress" id="adress" />
             </div>
             <div>
-              <label for="zipCode">Zip Code</label>
+              <label htmlFor="zipCode">Zip Code</label>
               <input type="text" name="zipCode" id="zipCode" />
             </div>
             <div>
-              <label for="city">City</label>
+              <label htmlFor="city">City</label>
               <input type="text" name="city" id="city" />
             </div>
             <div>
-              <label for="phone">Phone</label>
+              <label htmlFor="phone">Phone</label>
               <input type="text" name="phone" id="phone" />
             </div>
+          </div>
+          <div className="experiance">
+            <button type="button" id="addExperience" onClick={this.increaseJobForms}>
+              Add
+            </button>
+            {this.appendExperianceForm()}
           </div>
         </form>
       </div>
